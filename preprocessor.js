a18e087088564660625dc08ececee232472d4b55
Josh Thomas <josh@ionic.io>
2017-10-20 11:43:02 -0500

    fix(): update jest preprocess so that it works with older versions of node.

--- preprocessor.js
+++ preprocessor.js
@@ -2,7 +2,7 @@ const tsc = require('typescript');
 const tsConfig = require('./tsconfig.json');
 
 module.exports = {
-  process(src, path) {
+  process: function (src, path) {
     if (path.endsWith('.ts') || path.endsWith('.tsx')) {
       return tsc.transpile(src, tsConfig.compilerOptions, path, []);
     }
